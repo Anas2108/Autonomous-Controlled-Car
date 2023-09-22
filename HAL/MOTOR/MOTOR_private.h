848a6534b49db895b63a9972377bfca1a602e653
Anas hamed ahmed hamed <111208532+Anas2108@users.noreply.github.com>
2023-09-22 04:10:50 +0300

    Add files via upload

---
+++ HAL/MOTOR/MOTOR_private.h
@@ -0,0 +1,13 @@
+/*************************************************************/
+/*   Author    :  Anas Hamed                                 */
+/*   Date      :  25 Aug 2023                                */
+/*   Version   :  V01                                        */
+/*************************************************************/
+#ifndef HAL_MOTOR_MOTOR_PRIVATE_H_
+#define HAL_MOTOR_MOTOR_PRIVATE_H_
+
+
+
+
+
+#endif /* HAL_MOTOR_MOTOR_PRIVATE_H_ */
